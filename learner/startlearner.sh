385273ebc7ab7e7fe117bb289c118aa69dd9a9ac
hallonfras <isaacwesterman@gmail.com>
2025-04-07 23:07:07 +0200

    added args file

--- learner/startlearner.sh
+++
@@ -1,2 +0,0 @@
-!#/bin/bash
-java -jar TCP-Learner.jar state-fuzzer-server -connect mapper:18200 -tcpMapperAddress mapper:18200 -alphabet /config/alphabet.xml -output /app/output_folder/ -memQueryRuns 3
